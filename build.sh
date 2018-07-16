3ec9bcdd3cd1c21fbce5fd71fe170451ef57d3c9
John Lubke <jlubke@docutap.com>
2018-07-16 12:57:01 -0500

    test circle building

--- build.sh
+++ build.sh
@@ -15,7 +15,6 @@
 # Change the variables below:
 #   LABEL   : the repository label for the resulting image
 #   VERS    : the tags that should be applied to the image
-#   BASEURI : URI for where installation script and package are available
 #   PKGDELIVERY_PATH : path URI to the OE installation package: progress_<ver>_tar.gz
 #   INSTFILE_PATH    : path URI to the OE silent installation file
 ##
@@ -26,10 +25,9 @@ declare -a VERS
 
 LABEL="pscservices/pasoeprod"
 VERS=( 11.7.3 latest )
-BASEURI="http://172.17.0.1"
 
 # path to the installation file: lnx64.tar.gz
-PKGDELIVERY_PATH=PROGRESS_OE_11.7.3_LNX_64_UPDATE.tar.gz
+PKGDELIVERY_PATH='https://s3.amazonaws.com/release-phi-docutap/progress-artifacts/PROGRESS_OE_11.7.3_LNX_64_UPDATE.tar.gz'
 INSTFILE_PATH=prod_11.7.3_response.ini
 
 ################################################################################
@@ -41,37 +39,17 @@ for v in ${VERS[@]}; do
    BUILDTAGS+=" -t ${LABEL}:${v}";
 done
 
-export PKGURI="${BASEURI}:8000";
-export INSTURI="${BASEURI}:8001";
-
-# ncat - part of nmap
-ncat -lp 8000 < ${PKGDELIVERY_PATH} &
-nc_pid1=$!
-
-ncat -lp 8001 < ${INSTFILE_PATH} &
-nc_pid2=$!
+export TAR_PACKAGE=${PKGDELIVERY_PATH};
+export INSTALL_INI_SCRIPT=${INSTFILE_PATH};
 
 ( docker rmi -f ${RMILIST} 1>/dev/null 2>&1 );
 
 pushd docker; 
 docker build \
    --rm \
-   --build-arg INSTURI="${INSTURI}" \
-   --build-arg PKGURI="${PKGURI}" \
+   --build-arg INSTALL_INI_SCRIPT="${INSTALL_INI_SCRIPT}" \
+   --build-arg TAR_PACKAGE="${TAR_PACKAGE}" \
    ${BUILDTAGS} \
    .
 popd;
 
-# use this for build testing
-# docker run --rm -ti --name oepastest -e INSTURI -e PKGURI ${LABEL}:latest
-
-
-if [ ! -z $nc_pid1 ]; then
-        ( kill $nc_pid1 2>/dev/null || true )
-fi
-
-if [ ! -z $nc_pid2 ]; then
-        ( kill $nc_pid2 2>/dev/null || true )
-fi
-
-#EOF
